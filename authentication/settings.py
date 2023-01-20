584c1acd72b7d1f81e31bd11bb2f84956526ce6e
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2023-01-21 01:12:00 +0530

    feat: setup templates, added basic homepage layout with boilerplate in bootstrap

--- authentication/settings.py
+++ authentication/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3s$5xqx4xp5gkzn+#x$7x1tj)vy&ww)$e2mbi$8g+rart7vt7%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,9 @@ ROOT_URLCONF = 'authentication.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
