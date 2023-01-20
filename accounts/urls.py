06f40351ea56eaa76e1bfa65399398e898010caf
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2023-01-21 01:12:00 +0530

    feat: login and register functionality done

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from django.contrib.auth import decorators
+
 from .views import HomeView, UserRegisterView, UserLoginView
 
 app_name = 'home'
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
+    path('', decorators.login_required(HomeView.as_view(), login_url='accounts:login'), name='home'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('register/', UserRegisterView.as_view(), name='register'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import login
+from django.contrib.auth import login, decorators
 from django.urls import reverse_lazy
 from django.views.generic import FormView, TemplateView
 
@@ -11,24 +11,27 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # add any additional context data here
-        return context
 
+        # add any additional context data here
+        if self.request.user.is_authenticated:
+            context['user'] = self.request.user
+        return context
 
 
 class UserLoginView(FormView):
     form_class = UserLoginForm
     template_name = 'accounts/login.html'
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('accounts:home')
 
     def form_valid(self, form):
         login(self.request, form.get_user())
         return super().form_valid(form)
 
+
 class UserRegisterView(FormView):
     form_class = UserRegisterForm
     template_name = 'accounts/register.html'
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('accounts:home')
 
     def form_valid(self, form):
         user = form.save()
